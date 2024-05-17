be4028accb0415f01729f95a0bd57d75e18270e0
Ali El-esawy <73000699+alielesawy@users.noreply.github.com>
2024-05-17 19:20:05 +0000

    trying deploy

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,16 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Made with <span role="img">❤️</span> by {greeting.title}
+          Customized with {greeting.title}
+        </p>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          <a href="https://github.com/ashutosh1919/masterPortfolio/">
+            هذه نسخة من مستودع تجده في هذا الرابط
+          </a>
+        </p>
+        <p className="footer-text" style={{ color: props.theme.secondaryText }}>
+          اللهم انصر غزة وأهلها ، اللهم أعز الإسلام والمسلمين يا رب وحد كلمتهم
+          وارفع رايتهم وأنصرهم يا ذا القوةِ المتين.
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
